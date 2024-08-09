9d438b27c71910ddc3ef213f5e9a92a1d7d840f2
Gaston Herrlein <herrleingaston@gmail.com>
2024-08-09 18:21:22 +0200

    feat: se añade el endpoint al fichero de rutas

--- src/routes/anuncioRoutes.ts
+++ src/routes/anuncioRoutes.ts
@@ -1,5 +1,10 @@
 import express from 'express';
-import { getAnuncios, uploadImages, getAnunciosUsuario } from '../controllers/anuncioController';
+import {
+  getAnuncios,
+  uploadImages,
+  getAnunciosUsuario,
+  deleteAnuncio,
+} from '../controllers/anuncioController';
 import multer from 'multer';
 
 const router = express.Router();
@@ -8,5 +13,6 @@ const upload = multer({ storage: multer.memoryStorage() });
 router.get('/anuncios', getAnuncios);
 router.get('/perfil/:nombreUsuario/anuncios', getAnunciosUsuario);
 router.post('/anuncios/upload', upload.array('imagenes'), uploadImages);
+router.delete('/anuncios/:anuncioId', deleteAnuncio);
 
 export default router;
