4ab74861588a5d7177a819b335e87c44fa98f025
Gaston Herrlein <herrleingaston@gmail.com>
2024-08-21 03:54:20 +0200

    se crea utilidad con funciones auxiliares

---
+++ src/utils/helpers.ts
@@ -0,0 +1,21 @@
+//De un objeto devuelve un array con las claves de aquellos valores que sean undefined
+export const checkUndefined = (data: { [key: string]: string | undefined }): string[] => {
+  let isUndefined: string[] = [];
+  for (let key in data) {
+    if (data[key] === undefined) {
+      isUndefined.push(key);
+    }
+  }
+  return isUndefined;
+};
+
+//controller/authController linea 11
+export const isValidName = (name: string): Boolean => {
+  return !/^[a-zA-Z0-9_-]+$/.test(name);
+};
+
+//controller/authController linea 15
+//controller/userController linea 81
+export const isValidPassword = (password: string): Boolean => {
+  return password.length < 6;
+};
