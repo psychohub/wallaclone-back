d29078576e71f26accaf33c0d4afd1a567fd7b6b
Gaston Herrlein <herrleingaston@gmail.com>
2024-09-12 01:53:17 +0200

    feat: se crea metodo para crear url firmadas

--- src/controllers/s3Controller.ts
+++ src/controllers/s3Controller.ts
@@ -1,4 +1,5 @@
-import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 import s3Client from '../config/s3';
 
@@ -15,4 +16,9 @@ export const uploadFileToS3 = async (file: Express.Multer.File, filename: string
     console.log(err);
     throw Error('No se pudo subir la imagen al repositorio');
   }
+};
+
+export const presignedUrl = ({ key }: { key: string }) => {
+  const command = new GetObjectCommand({ Bucket: process.env.AWS_BUCKET_NAME, Key: key });
+  return getSignedUrl(s3Client, command, { expiresIn: 3600 }); // expires in 1hs
 };
