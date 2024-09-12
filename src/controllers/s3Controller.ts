4a9e0b1c65b49551477e4026c540ca7e92efbadd
Gaston Herrlein <herrleingaston@gmail.com>
2024-09-12 03:10:16 +0200

    feat: se crea metodo para eliminar objeto de s3

--- src/controllers/s3Controller.ts
+++ src/controllers/s3Controller.ts
@@ -1,4 +1,4 @@
-import { PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 import s3Client from '../config/s3';
@@ -21,4 +21,16 @@ export const uploadFileToS3 = async (file: Express.Multer.File, filename: string
 export const presignedUrl = ({ key }: { key: string }) => {
   const command = new GetObjectCommand({ Bucket: process.env.AWS_BUCKET_NAME, Key: key });
   return getSignedUrl(s3Client, command, { expiresIn: 3600 }); // expires in 1hs
+};
+
+export const deleteFile = async (filename: string) => {
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: filename,
+  };
+  try {
+    return await s3Client.send(new DeleteObjectCommand(params));
+  } catch (error) {
+    console.log(error);
+  }
 };
